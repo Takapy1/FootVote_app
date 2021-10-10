3a794755d30cf12d38621deab3d9b2afe8ed844d
Takapy1 <takahirog1109mai@gmail.com>
2021-10-10 12:09:40 +0900

    [Create] Type Game and GetGame func without DB

--- server/controllers/games.go
+++ server/controllers/games.go
@@ -5,11 +5,12 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/Takapy1/FootVote/server/models"
 )
 
 func GetGames(c *gin.Context) {
-	fmt.Println("Hello getGames") //terminal output
-	c.JSON(http.StatusOK, gin.H{ "data": "hello getGames" })
+	games := models.GetGames()
+	c.JSON(http.StatusOK, gin.H{ "data": games })
 }
 
 func GetGame(c *gin.Context) {
